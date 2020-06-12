1db78854279cf3f779db8462d30a606a6a6317a6
LadyDeveloper <anhacorrea@hotmail.com>
2020-06-12 07:24:16 -0700

    Passport

--- models/user.js
+++ models/user.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
+    //Username & Password removed due to the passport-local-mongoose, which will handle it with hashing and salting
+    // username: {
+    //     type: String,
+    //     required: true,
+    //     unique: true
+    // },
+    // password: {
+    //     type: String,
+    //     required: true
+    // },
     admin: {
         type: Boolean,
         default: false
     }
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
